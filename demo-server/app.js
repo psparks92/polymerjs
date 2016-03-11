18ca982fe1d6148148871ff359d28918f84ab5d2
Peter Sparks HCO <psparks1992@gmail.com>
2016-03-11 12:17:49 -0600

    initial commit

---
+++ demo-server/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by psparks on 12/31/2015.
+ */
+var express = require("../node_modules/express");
+var app = express();
+var path = require("path");
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/../app/index.html'));
+});
+app.use(express.static(path.join(__dirname, '../')));
+var http = require('http').Server(app);
+http.listen(8080);
